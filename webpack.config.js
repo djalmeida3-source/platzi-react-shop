e36a096eee04ec8d3a19e630b4d0f32e21c56369
David Almeida <djalmeida3@gmail.com>
2022-06-26 21:32:42 -0400

    Add deploy configurations

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        filename: 'platzi-react-shop/bundle.js',
+        publicPath: '/platzi-react-shop/'
     },
     mode: 'development',
     resolve: {
